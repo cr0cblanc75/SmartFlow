0a55b0b7cce0d9cdb12685eec47b8273efac4c58
cr0cblanc75 <crocblanc71@gmail.com>
2026-06-29 11:20:45 +0200

    Add SVG assets for Clock, Home Button, and Logo Navbar; update HomeScreen component to include new buttons

--- src/app/(main)/home_map.tsx
+++ src/app/(main)/home_map.tsx
@@ -5,6 +5,13 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Pressable } from "react-native";
 import { useRouter } from "expo-router";
 
+import HomeButton from "@/assets/Home_Button.svg";
+import VectorButton from "@/assets/Vector.svg";
+import LightningButton from "@/assets/Lightning.svg";
+import ProfileButton from "@/assets/Profile.svg";
+import LogoNavbar from "@/assets/Logo-NavBar.svg";
+import ClockButton from "@/assets/Clock.svg";
+
 import { AnimatedScreen } from "@/components/AnimatedScreen";
 import Map from "@/leaflet/leaflet";
 
@@ -21,8 +28,13 @@ export default function HomeScreen() {
             </View>
 
             {/* GREEN BOX OVERLAY */}
-            <View style={[styles.bottomBox, {backgroundColor: "green", paddingBottom: safeAreaInsets.bottom + 10,},]}>
-                <Pressable style={[styles.button, { backgroundColor: theme.ButtonBackground }]} onPress={() => router.push("/(main)/home_map")}>
+            <View style={[styles.bottomBox, { backgroundColor: "green", paddingBottom: safeAreaInsets.bottom + 10 }]}>
+                <Pressable onPress={() => router.push("/(main)/home_map")}>
+                    <HomeButton width={"100%"} height={25} preserveAspectRatio="xMidYMid meet" color={theme.MainTextWhite} />
+                </Pressable>
+
+                <Pressable onPress={() => router.push("/(main)/home_map")}>
+                    <VectorButton width={"100%"} height={25} preserveAspectRatio="xMidYMid meet" color={theme.MainTextWhite} />
                 </Pressable>
             </View>
         </AnimatedScreen>
@@ -37,26 +49,11 @@ const styles = StyleSheet.create({
         right: 0,
 
         minHeight: 120,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
 
         padding: Spacing.two,
         zIndex: 10,
         elevation: 10,
-    },
-
-    button: {
-        width: "100%",
-        height: 40,
-
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        alignSelf: "center",
-
-        paddingVertical: Spacing.two,
-        paddingHorizontal: Spacing.five,
-
-        borderRadius: 12,
     },
 });
