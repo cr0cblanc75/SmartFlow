7efd00d169641f3216e35cae5d1ec20ce0918b1e
cr0cblanc75 <crocblanc71@gmail.com>
2026-07-02 10:10:21 +0200

    feat: Implement waypoint functionality with dynamic map centering and info bubble display

--- src/components/waypoints/waypoints_frame.tsx
+++ src/components/waypoints/waypoints_frame.tsx
@@ -1,21 +1,22 @@
 import Ping from "@/assets/Ping.svg";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import { ThemedText } from "../themed-text";
 import { Spacing, FontWeight } from "@/constants/theme";
 import { useTheme } from "@/hooks/use-theme";
 
 type WaypointsFrameProps = {
     name?: string;
+    onPress: () => void;
 };
 
-export function WaypointsFrame({ name = "Waypoint name Not Found" }: WaypointsFrameProps) {
+export function WaypointsFrame({ name = "Waypoint name Not Found", onPress }: WaypointsFrameProps) {
     const theme = useTheme();
 
     return (
-        <View style={[styles.Frame, { backgroundColor: theme.MainBackground100 }]}>
+        <Pressable style={[styles.Frame, { backgroundColor: theme.MainBackground100 }]} onPress={onPress}>
             <Ping width={20} height={25} preserveAspectRatio="xMidYMid meet" />
             <ThemedText style={[styles.Name, { color: theme.MainTextBlack }]}>{name}</ThemedText>
-        </View>
+        </Pressable>
     );
 }
 
